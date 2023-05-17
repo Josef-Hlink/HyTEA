1c6e2fd75399116248f815a1fcd18bc8e0683496
Levi Peeters <levipeeters1@hotmail.com>
2023-05-17 13:45:29 +0200

    Implement EA with simple evaluation function

--- hytea/algorithm.py
+++ hytea/algorithm.py
@@ -35,49 +35,98 @@ class EvolutionaryAlgorithm:
         """
         Run the evolutionary algorithm.
         """
-        pass
+        self.initialize_population()
+
+        for i in range(self.generations):
+            fitness_values = self.evaluate_population()
+            print(f"Generation {i}: {np.mean(fitness_values)}")
+            selected_candidates = self.select(fitness_values)
+            crossovered_candidates = self.crossover(selected_candidates)
+            mutated_candidates = self.mutate(crossovered_candidates)
+            self.replace(mutated_candidates)
+        
+        best_candidate = self.get_best_candidate()
+        print(best_candidate)
 
     def initialize_population(self) -> None:
         """
         Initialize the population with random bitstrings.
         """
-        self.population = np.random.randint(2, size=(self.population_size, self.candidate_size))
+        self.population = np.random.randint(2, size=(self.population_size, self.candidate_size), dtype=np.uint8)
     
     def evaluate_population(self) -> np.ndarray:
         """
         Evaluate the population by training and testing the bitstrings.
         """
-        pass
+
+        fitness_values = np.sum(self.population, axis=1)
+        return fitness_values
 
     def select(self, fitness_values: np.ndarray) -> np.ndarray:
         """
-        Select the best candidates from the population.
+        Select the best candidates from the population using tournament selection.
         """
-        pass
+        selected_candidates = np.empty((self.mu_, self.candidate_size))
+
+        for i in range(self.mu_):
+            tournament_indices = np.random.choice(len(self.population), self.pool_size, replace=False)
+            tournament = self.population[tournament_indices]
+            best_individual_index = np.argmax(fitness_values[tournament_indices])
+            best_individual = tournament[best_individual_index]
+            selected_candidates[i] = best_individual
+
+        return selected_candidates
+
 
     def crossover(self, selected_candidates: np.ndarray) -> np.ndarray:
         """
         Perform crossover on the selected candidates.
         """
-        pass
+        crossovered_candidates = np.empty((self.lambda_, selected_candidates.shape[1]))
+
+        for i in range(0, self.lambda_, 2):
+            parent1_index = i % self.mu_
+            parent2_index = (i + 1) % self.mu_
+
+            parent1 = selected_candidates[parent1_index]
+            parent2 = selected_candidates[parent2_index]
+
+            crossover_point = np.random.randint(1, len(parent1) - 1)
+            offspring1 = np.concatenate((parent1[:crossover_point], parent2[crossover_point:]))
+            offspring2 = np.concatenate((parent2[:crossover_point], parent1[crossover_point:]))
+
+            crossovered_candidates[i] = offspring1
+            crossovered_candidates[i + 1] = offspring2
+        
+        return np.concatenate((selected_candidates, crossovered_candidates))
 
     def mutate(self, crossovered_candidates: np.ndarray) -> np.ndarray:
         """
         Mutate the crossovered candidates.
         """
-        pass
+        mask = np.random.rand(*crossovered_candidates.shape) < self.mutation_rate
+        mutated_population = np.where(mask, 1-crossovered_candidates, crossovered_candidates)
+
+        return mutated_population
 
     def replace(self, mutated_candidates: np.ndarray) -> None:
         """
         Replace the old population with the new population.
         """
-        pass
+        self.population = mutated_candidates
 
     def get_best_candidate(self) -> np.ndarray:
         """
         Return the best candidate from the population.
         """
-        pass
+        fitness_values = self.evaluate_population()
+        best_candidate_index = np.argmax(fitness_values)
+        best_candidate = self.population[best_candidate_index]
+        return best_candidate
+
+if __name__=="__main__":
+    ea = EvolutionaryAlgorithm(50, 18)
+    ea.run()
 
                 
 
